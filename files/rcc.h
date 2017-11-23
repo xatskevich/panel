a1f0c9d08121ae986be136575ca3ff03174dbe94
Xatskevich <xatskevich@gmail.com>
2017-11-23 16:52:26 +0300

    convert to Keil

---
+++ files/rcc.h
@@ -0,0 +1,8 @@
+
+#ifndef __RCC_H
+#define __RCC_H
+
+void Init_RCC(void);
+void Init_IWDG(void);
+
+#endif
---
+++ files/timer.h
@@ -0,0 +1,11 @@
+
+#ifndef __TIMER_H
+#define __TIMER_H
+
+void Init_Timer(void);
+void Send_One(void);
+void Send_Nul(void);
+void delay_ms (uint16_t dl);
+void delay_us (uint32_t dl);
+
+#endif
